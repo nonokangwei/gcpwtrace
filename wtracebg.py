4f381ad48813292849858ada0ae07673a8793382
nonokangwei <nonokangwei@163.com>
2021-05-09 23:59:09 +0800

    Update wtracebg.py

--- wtracebg.py
+++ wtracebg.py
@@ -3,145 +3,145 @@ import datetime
 import time
 import pandas
 from math import ceil
-from subprocess import PIPE, run
+import subprocess
 from concurrent.futures import ThreadPoolExecutor
 from google.cloud import bigquery
 
-# sampledata = """The trace ran on: 62.214.98.9 (versatel-fra3:8881:xp)
-#   Time elapsed: 183 usecs
-#   Time elapsed: 981 usecs
-#   Response code: 200
-#   Time elapsed (after TCP handshake): 45466 usecs"""
-
-# Construct the argument parser
-ap = argparse.ArgumentParser()
-
-# Add the arguments to the parser
-ap.add_argument("-c", "--countrycode", required=True,
-   help="please check out the available country code with --list countrycode")
-ap.add_argument("-i", "--interval", required=True,
-   help="set the trace interval")
-ap.add_argument("-p", "--parrallel", required=True,
-   help="set concurrency which can not greater than 32")
-ap.add_argument("-e1", "--endpoint1", required=True,
-   help="set trace destination with pattern http://")
-ap.add_argument("-e2", "--endpoint2", required=True,
-   help="set trace destination with pattern http://")
-args = vars(ap.parse_args())
-
-# Init variable
-country_code = args["countrycode"]
-max_worker = int(args["parrallel"])
-interval = int(args["interval"])
-dest_url_a = args["endpoint1"]
-dest_url_b = args["endpoint2"]
-
-def get_wtrace_agent_list(country_code, table_id, ip_version="ipv4"):
-    # Construct a BigQuery client object.
-    client = bigquery.Client()
-
+def get_wtrace_agent_list(bq_client, country_code, table_id, ip_version='ipv4'):
     # Init Query
     query = """
     SELECT ARRAY_AGG(ip_address LIMIT 1)[OFFSET(0)] as address, country_code as country, isp, metro
-    FROM `project-wekang-poc.wtrace.agentinfor`
-    WHERE country_code = {} and ip_version={}
+    FROM `{}`
+    WHERE country_code = "{}" and ip_version="{}"
     GROUP BY isp, metro, country
-""".format('"'+country_code+'"', '"'+ip_version+'"')
+""".format(table_id, country_code, ip_version)
 
-    query_job = client.query(query)
+    query_job = bq_client.query(query)
     rows = query_job.to_dataframe()
-    
-    # Close the BigQuery client object.
-    client.close()
 
     return rows
 
-def run_wtrace(country_code, dest_url, bg_client, table_id, agent_ip):
-    latency_result = {}
-    rows_to_insert = []
-    wtrace_cmd = "/google/data/ro/teams/internetto/wtrace --nowait --ip_version=4 --agent=" + agent_ip["address"] + " " + dest_url
-#    cmd = 'ssh ' + glinux_host + ' "' + wtrace_cmd + '"' + '|grep "Response code\|Time elapsed\|The trace ran on"'
-    cmd = wtrace_cmd  + '|grep "Response code\|Time elapsed\|The trace ran on"'
-    print(cmd)
-    ret = run(cmd, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
-    # Parse the cmd result to linesç
-    ret_splitlines = ret.stdout.splitlines()
-    # print(ret_splitlines)
-    # Parse the result with schema
-    for i in range(0,len(ret_splitlines)):
-        if i == 0:
-            latency_result["agent_ip"] = ret_splitlines[i].split(":")[1].split()[0].strip()
-        elif i == 1:
-            latency_result["dns_latency"] = int(ret_splitlines[i].split(":")[1].split()[0].strip())
-        elif i == 2:
-            latency_result["tcp_latency"] = int(ret_splitlines[i].split(":")[1].split()[0].strip())
-        elif i == 3:
-            latency_result["response_code"] = int(ret_splitlines[i].split(":")[1].strip())
-        elif i == 4:
-            latency_result["http_latency"] = int(ret_splitlines[i].split(":")[1].split()[0].strip())
-    # Append Countrycode and Datetime information
-    latency_result["country_code"] = country_code
-    latency_result["datetime"] = str(datetime.datetime.utcnow())
-    latency_result["isp"] = agent_ip["isp"]
-    latency_result["metro"] = agent_ip["metro"]
-    latency_result["dest_url"] = dest_url
-    print(latency_result)
-    # Streaming the metric to BQ
-    rows_to_insert.append(latency_result)
-    errors = client.insert_rows_json(table_id,rows_to_insert) # Make an API request.
-    if errors == []:
-        print("New rows have been added.")
-    else:
-        print("Encountered errors while inserting rows: {}".format(errors))
-    # Check out the lantency_result
-    # Print(latency_result)
+def extract_result(lines):
+    if len(lines) != 7:
+        raise Exception('error result format, expected length: 7, get length: ', len(lines))
 
+    result = {}
+    result['agent_ip'] = lines[0].split(':')[1].split()[0].strip()
+    result['dns_latency_us'] = int(lines[1].split(':')[1].split()[0].strip())
+    result['remote_ip'] = lines[2].split('Using IP address:')[1].split()[0].strip()
+    result['remote_ip_info'] = lines[2].split('Using IP address:')[1].split()[1].strip().replace('(','').replace(')','')
+    result['tcp_latency_us'] = int(lines[3].split(':')[1].split()[0].strip())
+    #different agent return following line in different order, need to check keyword to map to current field
+    for line_no in range(4,7):
+        if lines[line_no].find('rtt') != -1:
+            result['ping_min_latency_ms'] = float(lines[line_no].split('=')[1].replace('ms', '').split('/')[0].strip())
+            result['ping_avg_latency_ms'] = float(lines[line_no].split('=')[1].replace('ms', '').split('/')[1].strip())
+            result['ping_max_latency_ms'] = float(lines[line_no].split('=')[1].replace('ms', '').split('/')[2].strip())
+        elif lines[line_no].find('Response') != -1:
+            result['response_code'] = int(lines[line_no].split(':')[1].strip())
+        elif lines[line_no].find('handshake') != -1:
+            result['http_latency_us'] = int(lines[line_no].split(':')[1].split()[0].strip())
 
+    return result
+ 
+def run_wtrace(country_code, dest_url, bq_client, table_id, agent_ip, run_info, pid, agent_num, loop):
+    wtrace_cmd = '/google/data/ro/teams/internetto/wtrace --nowait --ip_version=4 --agent={} {}'.format(agent_ip['address'], dest_url)
+    wtrace_cmd += ' | grep "Response code\|Time elapsed\|The trace ran on\|rtt min/avg/max\|Using IP address"'
+    try:
+        wtrace_output = subprocess.run(wtrace_cmd, stdout=subprocess.PIPE, shell=True, universal_newlines=True)
+        ret_splitlines = wtrace_output.stdout.splitlines()
+        latency_result = extract_result(ret_splitlines)
+        latency_result.update(run_info)
+        print('[loop {}: {}/{}]: {}'.format(loop+1, pid, agent_num, latency_result))
+        bq_client.insert_rows_json(table_id,[latency_result])
+    except Exception as e:
+        print('wtrace command output error: ', ret_splitlines, e)
 
-def start_wtrace_thread_pool(max_worker, country_code, dest_url, bg_client, table_id, num_loop, agent_list):
-    if num_loop == 1:
+def start_wtrace_thread_pool(max_worker, country_code, dest_url, bq_client, table_id, num_loop, agent_list, run_info, interval, max_hours):
+    #set maximum run time to 1 day, due to gcert certificate is only valid in 19 hours
+    agent_num = agent_list.shape[0]
+    endtime = datetime.timedelta(hours=max_hours) + datetime.datetime.utcnow()
+    for loop in range(num_loop):
+        print('===================================== LOOP {} ====================================='.format(loop+1))
         pool = ThreadPoolExecutor(max_workers=max_worker, thread_name_prefix="wtrace_")
+        pid = 1
         for index, agent_ip in agent_list.iterrows():
-            pool.submit(run_wtrace, country_code, dest_url, bg_client, table_id, agent_ip)
-
+            if datetime.datetime.utcnow() < endtime:
+                run_info['isp'] = agent_ip['isp']
+                run_info['metro'] = agent_ip['metro']
+                pool.submit(run_wtrace, country_code, dest_url, bq_client, table_id, agent_ip, run_info, pid, agent_num, loop)
+                pid += 1
+                time.sleep(interval)
         pool.shutdown(wait=True)
-        print('threadPool shutdown')
-    else:
-        # for j in range(0, num_loop):
-        pool = ThreadPoolExecutor(max_workers=max_worker, thread_name_prefix="wtrace_")
-        k = 0
-        for index, agent_ip in agent_list.iterrows():
-            pool.submit(run_wtrace, country_code, dest_url, bg_client, table_id, agent_ip)
-            k = k + 1
-            if k % 30 == 0 or k == agent_list.shape[0]:
-                pool.shutdown(wait=True)
-                print('threadPool shutdown')
-                pool = ThreadPoolExecutor(max_workers=max_worker, thread_name_prefix="wtrace_")
 
-# Init tableid
-latency_table_id = "project-wekang-poc.wtrace.latencymetric"
-agent_table_id = "project-wekang-poc.wtrace.agentinfor"
+def main():
+    # Construct the argument parser
+    ap = argparse.ArgumentParser()
 
-agent_list = get_wtrace_agent_list(country_code, agent_table_id)
-print(agent_list.shape[0])
-# Init max_worker
-if agent_list.shape[0] > 30:
-    max_worker = 30
-    num_loop = int(ceil(agent_list.shape[0]/30))
-else:
-    max_worker = agent_list.shape[0]
-    num_loop = 1
+    # Add the arguments to the parser
+    ap.add_argument("-c", "--countrycode", required=True,
+       help="please check out the available country code with --list countrycode")
+    ap.add_argument("-i", "--interval", required=True,
+       help="set the trace interval")
+    ap.add_argument("-p", "--parrallel", required=True,
+       help="set concurrency which can not greater than 32")
+    ap.add_argument("-e", "--endpoint", required=True,
+       help="set trace destination with pattern http://")
+    ap.add_argument("-l", "--loop", default=1,
+       help="set test loop nuber")
+    ap.add_argument("-m", "--max_run_hours", default=19,
+       help="set test loop nuber")
+    args = vars(ap.parse_args())
 
-print(num_loop)
+    # Init variable
+    country_code = args["countrycode"]
+    max_worker = int(args["parrallel"])
+    interval = int(args["interval"])
+    dest_url = args["endpoint"]
+    num_loop = int(args["loop"])
+    max_hours = int(args["max_run_hours"])
+    run_id = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
 
-endtime = datetime.timedelta(days=1) + datetime.datetime.utcnow()
-while(datetime.datetime.utcnow() <= endtime):
+    # Init tableid
+    latency_table_id = "lxd-project.wtrace.run"         #<==== modify to your table
+    agent_table_id = "lxd-project.wtrace.agent_info"    #<==== modify to your table
+
+    schema = [
+        bigquery.SchemaField('run_id', 'STRING'),
+        bigquery.SchemaField('agent_ip', 'STRING'),
+        bigquery.SchemaField('remote_ip','STRING'),
+        bigquery.SchemaField('remote_ip_info','STRING'),
+        bigquery.SchemaField('dns_latency_us', 'INTEGER'),
+        bigquery.SchemaField('ping_min_latency_ms','FLOAT'),
+        bigquery.SchemaField('ping_avg_latency_ms','FLOAT'),
+        bigquery.SchemaField('ping_max_latency_ms','FLOAT'),
+        bigquery.SchemaField('tcp_latency_us', 'INTEGER'),
+        bigquery.SchemaField('http_latency_us','INTEGER'),
+        bigquery.SchemaField('response_code','INTEGER'),
+        bigquery.SchemaField('country_code','STRING'),
+        bigquery.SchemaField('datetime','DATETIME'),
+        bigquery.SchemaField('isp','STRING'),
+        bigquery.SchemaField('metro','STRING'),
+        bigquery.SchemaField('dest_url','STRING')
+    ]
+
     # Construct a BigQuery client object.
-    client = bigquery.Client()
-    # Start the metricmonitor test
-    start_wtrace_thread_pool(max_worker, country_code, dest_url_a, client, latency_table_id, num_loop, agent_list)
-    start_wtrace_thread_pool(max_worker, country_code, dest_url_b, client, latency_table_id, num_loop, agent_list)
-    # Wait for interval
-    time.sleep(60)
-    # Close the BigQuery client object.
+    client = bigquery.Client(location='asia-east1')
+    #create table
+    table = bigquery.Table(latency_table_id, schema=schema)
+    client.create_table(table, exists_ok=True)
+
+    agent_list = get_wtrace_agent_list(client, country_code, agent_table_id)
+
+    run_info = {
+        'run_id': run_id,
+        'country_code': country_code,
+        'datetime': str(datetime.datetime.utcnow()),
+        'dest_url': dest_url
+    }
+
+    start_wtrace_thread_pool(max_worker, country_code, dest_url, client, latency_table_id, num_loop, agent_list, run_info, interval, max_hours)
     client.close()
+
+if __name__ == '__main__':
+    main()
