964469ecb23007e8b10e4fe9bd58238aafb9a5f7
Dave Chen <hzchen@google.com>
2022-02-21 17:57:09 +0800

    update readme, config.ini, run.sh

--- pre_warm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-target="https://showbizapcdn.m2time.net/gameres/trialres/800829/1602/f0c611e2b983b55c53d4e66a6405590b/1644373265/lbres.zip"
-country_code="JP,KR,TW,HK,SG,TH,VN,PH,PK,BD,KR,ID,IN,MA,US,DE,FR,BR,RU,CA,IT,ES,PL,RO,NL,BE,GR,CZ"
-# country_code="PK"
-# target="https://coscloud.99.com/43f2bdd810c750ec7b7fedee2d1219fa/dcloud_default_component.zip"
-python wtrace.py -c ${country_code} -i 5 -n 10 -l 1 -e ${target}
-#python wtrace.py -c ${country_code} -i 5 -n 100 -l 5 -e ${target2}
--- run.sh
+++ run.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-#target1="http://d12x2nvgm9y53f.cloudfront.net/public/TencentMeeting.dmg"
 target="https://www1.totorochina.com/public/TencentMeeting.dmg"
-country_code="JP,KR,TW,HK,SG,TH,VN,PH,ID,MA,US,DE,FR,BR,RU,CA,IT"
-# country_code="PK"
-# target="https://coscloud.99.com/43f2bdd810c750ec7b7fedee2d1219fa/dcloud_default_component.zip"
+# country_code="JP,KR,TW,HK,SG,TH,VN,PH,ID,MA,US,DE,FR,BR,RU,CA,IT"
+country_code="HK,TW,SG"
 python wtrace.py -c ${country_code} -i 5 -n 100 -l 5 -e ${target}
-#python wtrace.py -c ${country_code} -i 5 -n 100 -l 5 -e ${target2}
