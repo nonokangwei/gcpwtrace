3a39fda0f26fad73f34c8fa65bc5a03e4a4f26f9
Dave Chen <hzchen@google.com>
2022-09-05 10:38:19 +0800

    add example.ipynb

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # target1="http://47.254.70.156/" # west
 # target2="http://47.253.54.30/" # east
-target1="http://8.219.85.89/gcp_gaming.pdf" # SG
-target2="http://8.209.247.102/gcp_gaming.pdf" # JP
-target3="http://47.254.134.72/gcp_gaming.pdf" # Frankfurt
+target1="http://35.88.2.220/" # AWS Oregon
+target2="http://34.168.68.249/" # GCP Oregon
+# target3="http://47.254.134.72/gcp_gaming.pdf" # Frankfurt
 # target4="" # JP
 # target5="" # Frankfurt
 # target6="" # Frankfurt
@@ -17,8 +17,11 @@ country_code1="ID,TH,VN,MY,PH,SG,MM,KH,LA,BN"
 country_code2="HK,TW,KR,JP,MO"
 country_code3="GE,GB,FR,IT,RU,ES,NL,CH,PL,SE"
 
-# country_code="US"
-python -u wtrace.py -c ${country_code1} -i 5 -n 100 -p 32 -e ${target1}
-python -u wtrace.py -c ${country_code2} -i 5 -n 100 -p 32 -e ${target2}
-python -u wtrace.py -c ${country_code3} -i 5 -n 100 -p 32 -e ${target3}
+country_code="US"
+python -u wtrace.py -c ${country_code} -i 5 -n 200 -p 32 -e ${target1}
+python -u wtrace.py -c ${country_code} -i 5 -n 200 -p 32 -e ${target2}
+
+python -u wtrace.py -c ${country_code3} -i 5 -n 200 -p 32 -e ${target1}
+# python -u wtrace.py -c ${country_code2} -i 5 -n 100 -p 32 -e ${target2}
+# python -u wtrace.py -c ${country_code3} -i 5 -n 100 -p 32 -e ${target3}
 # python -u wtrace.py -c ${country_code} -i 5 -l 1 -p 32 -e ${target}
